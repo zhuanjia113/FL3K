7c15c3a4fb205dcb926a2f73f9d1a70c228d1bd3
zhuanjia113 <271255751@qq.com>
2024-12-26 09:17:47 +0800

    Delete Get_3DKeyPoint_FL3K.m

--- Get_3DKeyPoint_FL3K.m
+++
@@ -1,9 +0,0 @@
-function  predict_3DKeypoint = Get_3DKeyPoint_FL3K(points_XYZ)
-
-
- salient_value = GetSalientValue_PointCloud(points_XYZ);                      % compute the geometric and high-level semantic saliency
-
-
- predict_3DKeypoint = GetKeypoint_SalientMap(points_XYZ,salient_value);       % Generate the stable keypoint from the saient value
- 
-
